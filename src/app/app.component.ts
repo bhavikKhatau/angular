558b566665ff41eabd7759174d7519ba91c8ea54
unknown <khataubhavik@gmail.com>
2022-06-15 04:07:04 -0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title="Todo list in Angular";
+  list:any[]=[];
+  addtesk(item:string){
+    this.list.push({id:this.list.length,name:item});
+  }
+  removetesk(id:number){
+    this.list.splice(id,id);
+  }
+}
